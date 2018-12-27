d3f062ab5ce423a4f967d8628323d17c3cb4f0e7
Константин Михайлович Тихомиров <15783@tyazhmash.com>
2018-12-27 14:42:57 +0400

    Исправление ошибки

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ var zhim = [26.25, 28.125, 30, 31.875, 33.75, 35.625, 37.5];
 var taga = [40, 41.875, 43.75, 45.625, 47.5, 49.375, 51, 25];
 var pric = [31.25, 33.125, 35, 36.875, 38.75, 40.625, 42.5];
 
-var result = zhim.map(value => utils.CalcWeight(value, 5)).map(array => array.map(value => utils.WeightToText(value)));
+var result = zhim.map(value => utils.CalcExerciseSetsWeight(value, 5)).map(array => array.map(value => utils.WeightToText(value)));
 console.log(JSON.stringify(result));
 console.log(process.execPath);
