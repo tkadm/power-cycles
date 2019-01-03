a9b333e55c91e744fb7fa192506ebda6f023350c
Тихомиров Константин <tdka97@yandex.ru>
2019-01-03 15:08:30 +0400

    Создана процедура нормализации

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-import { IRoot, IWorkout, ICycleMain, ICycleNested } from "./root";
+import { IRoot, IWorkout, ICycleMain, ICycleNested, ITransform } from "./root";
 
 export enum TransformType { linear = "linear" };
 export enum TransformRelativity { absolute = "absolute", procent = "procent" };
@@ -6,6 +6,9 @@ export enum NestedCycleBaseStage { prev = "prev", next = "next" }
 export interface IDictionary<Type> {
     [index: string]: Type;
 }
+export type MainTransformRoutine = (index: number, base_weight: number) => number;
+export type NestedTransformRoutine = (index: number, prev_weight: number, next_weight: number) => number;
+
 
 export interface IInput {
     date_begin: Date;
@@ -23,16 +26,15 @@ export function ComputeWorkouts(input: IInput, root: IRoot): Array<IWorkoutDayTa
     for (let wo_name in wo_dic) {
         let wo: IWorkout = wo_dic[wo_name];
         NormilizeWorkout(wo, root);
+
     }
     return result;
 }
 
 function NormilizeWorkout(workout: IWorkout, root: IRoot): void {
     if (!workout.cycle) return;
-    let cycle: ICycleMain = root.cycles[workout.cycle];
+    let cycle: ICycleMain | ICycleNested = root.cycles[workout.cycle];
     if (!cycle) throw "Ссылка на несуществующее в конфигурации имя цикла [" + workout.cycle + "]";
-    /*    begin_cycle_level: number;
-    start_date_offset: 0; */
     if (!workout.base_stage) workout.base_stage = cycle.base_stage;
     if (!workout.stage_period) workout.stage_period = cycle.stage_period;
     if (!workout.stages) workout.stages = cycle.stages;
@@ -41,12 +43,25 @@ function NormilizeWorkout(workout: IWorkout, root: IRoot): void {
         if (!workout.transform.type) workout.transform.type = cycle.transform.type;
     }
     if (!workout.nested) workout.nested = cycle.nested; else {
-        let cycle_nested: ICycleNested = cycle.nested;
+        cycle = cycle.nested;
         let workout_nested: ICycleNested = workout.nested;
-        while (cycle_nested) {
-
+        while (cycle) {
+            if (!workout_nested.base_stage) workout_nested.base_stage = cycle.base_stage;
+            if (!workout_nested.stage_periods) workout_nested.stage_periods = cycle.stage_periods;
+            if (!workout_nested.stages) workout_nested.stages = cycle.stages;
+            if (!workout_nested.transform) workout_nested.transform = cycle.transform; else {
+                if (!workout_nested.transform.relativity) workout_nested.transform.relativity = cycle.transform.relativity;
+                if (!workout_nested.transform.type) workout_nested.transform.type = cycle.transform.type;
+            }
+            if (!workout_nested.nested) {
+                workout_nested.nested = cycle.nested;
+                break;
+            }
             workout_nested = workout_nested.nested;
-            cycle_nested = cycle_nested.nested;
+            cycle = cycle.nested;
         }
     }
+}
+function CreateMainTransformRoutine(transform:ITransform): MainTransformRoutine {
+
 }
