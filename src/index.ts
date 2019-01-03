2e3eab00ea4ac883c2883511e7f1db87687f2f24
Тихомиров Константин <tdka97@yandex.ru>
2019-01-03 11:46:09 +0400

    Инициализация новой ветви

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import * as fs from "fs";
-import { IRoot, IDictionary } from "./root";
-import { Compute, IComputeData } from "./core";
 
-let content = fs.readFileSync(__dirname + "/../src/Data/config.json", "utf8");
-let config: IRoot = JSON.parse(content);
 
-let exes: IDictionary<number> = { exeA: 100, exeB: 105, exeC: 90 };
-let source: IComputeData = { cycle_id: "standard", exe_weights: exes, workout_id: "a-template" };
-let res = Compute(config, source);
+// let content = fs.readFileSync(__dirname + "/../src/Data/config.json", "utf8");
+// let config: IRoot = JSON.parse(content);
 
-console.log(JSON.stringify(res));
 
