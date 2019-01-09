4fdf35077e7354704bb65892000727739cd130f3
Тихомиров Константин <tdka97@yandex.ru>
2019-01-09 19:00:48 +0400

    Переделка

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { ICycleNested, ITransform } from "./root"
+
 export function DateCopy(source: Date, offset_days: number): Date {
     let result: Date = new Date(source.valueOf());
     result.setDate(result.getDate() + offset_days);
@@ -11,4 +13,74 @@ export function DateTrunc(source: Date): Date {
 
 export function DaysBetween(earlier: Date, later: Date): number {
     return (later.valueOf() - earlier.valueOf()) / (24 * 60 * 60 * 1000);
+}
+
+export enum TransformType { linear = "linear" };
+export enum TransformRelativity { absolute = "absolute", procent = "procent" };
+export enum NestedCycleBaseStage { prev = "prev", next = "next" }
+export interface IDictionary<Type> {
+    [index: string]: Type;
+}
+export type MainTransformRoutine = (index: number, base_weight: number) => number;
+export type NestedTransformRoutine = (index: number, prev_weight: number, next_weight: number) => number;
+
+function CreateMainTransformRoutine(transform: ITransform): MainTransformRoutine {
+    let result: MainTransformRoutine;
+    switch (transform.type) {
+        case TransformType.linear:
+            switch (transform.relativity) {
+                case TransformRelativity.absolute:
+                    result = (index, weight) => weight + index * transform.value
+                    break;
+                case TransformRelativity.procent:
+                    result = (index, weight) => weight + index * weight * transform.value / 100;
+                    break;
+                default: throw "Неизвестный relativity [" + transform.relativity + "]";
+            }
+            break;
+        default: throw "Неизвестный TransormType [" + transform.type + "]";
+    }
+    return result;
+}
+function CreateNestedTransformRoutine(transform: ITransform, nested_base_stage: NestedCycleBaseStage): NestedTransformRoutine {
+    let result: NestedTransformRoutine;
+    switch (transform.type) {
+        case TransformType.linear:
+            switch (transform.relativity) {
+                case TransformRelativity.absolute:
+                    switch (nested_base_stage) {
+                        case NestedCycleBaseStage.next:
+                            result = (index, prev_weight, next_weight) => next_weight + index * transform.value;
+                            break;
+                        case NestedCycleBaseStage.prev:
+                            result = (index, prev_weight, next_weight) => prev_weight + index * transform.value;
+                            break;
+                        default: throw "Неизвестное значение nested_base_stage [" + nested_base_stage + "]";
+                    }
+                    break;
+                case TransformRelativity.procent:
+                    switch (nested_base_stage) {
+                        case NestedCycleBaseStage.next:
+                            result = (index, prev_weight, next_weight) =>
+                                next_weight + index * next_weight * transform.value / 100;
+                            break;
+                        case NestedCycleBaseStage.prev:
+                            result = (index, prev_weight, next_weight) =>
+                                prev_weight + index * prev_weight * transform.value / 100;
+                            break;
+                        default: throw "Неизвестное значение nested_base_stage [" + nested_base_stage + "]";
+                    }
+                    break;
+                default: throw "Неизвестный TransormType [" + transform.type + "]";
+            }            
+    }
+    return result;
+}
+function CreateTransformRoutineArray(cycle: ICycleNested): Array<NestedTransformRoutine> {
+    let result: Array<NestedTransformRoutine> = [];
+    while (cycle) {
+        result.push(CreateNestedTransformRoutine(cycle.transform, cycle.base_stage));
+        cycle = cycle.nested;
+    }
+    return result;
 }
